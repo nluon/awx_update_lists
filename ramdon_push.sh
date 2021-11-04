fb0b1e6bf8c0accb8c9c41d0cc4e00376b7bbca5
nluon <56456819+nluon@users.noreply.github.com>
2021-11-04 10:54:23 -0700

    Delete ramdon_push.sh

--- ramdon_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sleep $[ ( $RAMDOM % 100 ) +5 ]s
-cd /srv/checkout/
-git config --global user.name "nluon"
-git config --global user.email "nimol.luonn@gmail.com"
-sudo git add .
-sudo git commit -m "hello"
-git push https://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@github.com/nluon/awx_update_lists.git
-exit 0
